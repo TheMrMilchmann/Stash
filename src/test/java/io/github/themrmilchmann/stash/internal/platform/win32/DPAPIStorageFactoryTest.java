a8c279db9fe44ace6887d4cdf0848fceea8358ed
Leon Linhart <themrmilchmann@gmail.com>
2022-08-13 13:33:07 +0200

    feat: add test coverage for storage factories

---
+++ src/test/java/io/github/themrmilchmann/stash/internal/platform/win32/DPAPIStorageFactoryTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2022 Leon Linhart
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package io.github.themrmilchmann.stash.internal.platform.win32;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public final class DPAPIStorageFactoryTest {
+
+    @EnabledOnOs(OS.WINDOWS)
+    @Test
+    public void testIsSupported_Windows() {
+        DPAPIStorageFactory factory = new DPAPIStorageFactory();
+        assertTrue(factory.isSupported());
+    }
+
+    @DisabledOnOs(OS.WINDOWS)
+    @Test
+    public void testIsSupported_Other() {
+        DPAPIStorageFactory factory = new DPAPIStorageFactory();
+        assertFalse(factory.isSupported());
+    }
+
+}
